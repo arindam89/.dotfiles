32d515ad5db42bbf3e8c27baffc4430b4e88fa29
Arindam Paul <arindampaul1989@gmail.com>
2024-10-04 14:11:50 +0530

    Added Karabiner Configuration as well to this .dotfiles repo

---
+++ karabiner/.config/karabiner/rules.ts
@@ -0,0 +1,101 @@
+import fs from "fs";
+import { KarabinerRules } from "./types";
+import { createHyperSubLayers, app, open, rectangle } from "./utils";
+
+const rules: KarabinerRules[] = [
+  // Define the Hyper key itself
+  {
+    description: "Hyper Key (⌃⌥⇧⌘)",
+    manipulators: [
+      {
+        description: "Caps Lock -> Hyper Key",
+        from: {
+          key_code: "caps_lock",
+          modifiers: {
+            optional: ["any"],
+          },
+        },
+        to: [
+          {
+            set_variable: {
+              name: "hyper",
+              value: 1,
+            },
+          },
+        ],
+        to_after_key_up: [
+          {
+            set_variable: {
+              name: "hyper",
+              value: 0,
+            },
+          },
+        ],
+        to_if_alone: [
+          {
+            key_code: "escape",
+          },
+        ],
+        type: "basic",
+      },
+      //      {
+      //        type: "basic",
+      //        description: "Disable CMD + Tab to force Hyper Key usage",
+      //        from: {
+      //          key_code: "tab",
+      //          modifiers: {
+      //            mandatory: ["left_command"],
+      //          },
+      //        },
+      //        to: [
+      //          {
+      //            key_code: "tab",
+      //          },
+      //        ],
+      //      },
+    ],
+  },
+  ...createHyperSubLayers({
+    spacebar: open(
+      "raycast://extensions/raycast/clipboard-history/clipboard-history"
+    ),  
+    s: app("Slack"),
+    n: app("Notion"),
+    g: app("Google Chrome"),
+    v: app("Visual Studio Code"),
+
+    // c = Musi*c* which isn't "m" because we want it to be on the left hand
+    c: {
+      p: {
+        to: [{ key_code: "play_or_pause" }],
+      },
+      n: {
+        to: [{ key_code: "fastforward" }],
+      },
+      b: {
+        to: [{ key_code: "rewind" }],
+      },
+    },
+  }),
+];
+
+fs.writeFileSync(
+  "karabiner.json",
+  JSON.stringify(
+    {
+      global: {
+        show_in_menu_bar: false,
+      },
+      profiles: [
+        {
+          name: "Default",
+          complex_modifications: {
+            rules,
+          },
+        },
+      ],
+    },
+    null,
+    2
+  )
+);
